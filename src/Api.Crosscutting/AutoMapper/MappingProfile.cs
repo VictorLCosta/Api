05a207264ac63e2a836a20bb41f1febb0e8b4df4
VictorLCosta <victorlc2019@outlook.com>
2021-12-24 19:35:21 -0300

    Mapping - Cep, City and State

--- src/Api.Crosscutting/AutoMapper/MappingProfile.cs
+++ src/Api.Crosscutting/AutoMapper/MappingProfile.cs
@@ -1,4 +1,7 @@
 using Api.Domain.DTO.User;
+using Api.Domain.DTO.Cep;
+using Api.Domain.DTO.City;
+using Api.Domain.DTO.State;
 using Api.Domain.Entities;
 using AutoMapper;
 
@@ -15,6 +18,42 @@ namespace Api.Crosscutting.AutoMapper
                 .ReverseMap();
 
             CreateMap<UpdateUserResultDto, User>()
+                .ReverseMap();
+
+
+            CreateMap<CityDto, City>()
+                .ReverseMap();
+
+            CreateMap<CreateCityDto, City>()
+                .ReverseMap();
+
+            CreateMap<UpdateCityDto, City>()
+                .ReverseMap();
+
+            CreateMap<CreateCityResultDto, City>()
+                .ReverseMap();
+
+            CreateMap<UpdateCityResultDto, City>()
+                .ReverseMap();
+
+
+            CreateMap<CepDto, Cep>()
+                .ReverseMap();
+
+            CreateMap<CreateCepDto, Cep>()
+                .ReverseMap();
+
+            CreateMap<UpdateCepDto, Cep>()
+                .ReverseMap();
+
+            CreateMap<CreateCepResultDto, Cep>()
+                .ReverseMap();
+
+            CreateMap<UpdateCepResultDto, Cep>()
+                .ReverseMap();
+
+            
+            CreateMap<StateDto, State>()
                 .ReverseMap();
 
         }
