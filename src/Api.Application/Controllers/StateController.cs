1ee1545cb38773dce5a27717c6b29e19a44b1ed3
VictorLCosta <victorlc2019@outlook.com>
2021-12-25 22:31:47 -0300

    StateController

---
+++ src/Api.Application/Controllers/StateController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Api.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Application.Controllers
+{
+    public class StateController : BaseApiController
+    {
+        private readonly IStateService _stateService;
+
+        public StateController(IStateService stateService)
+        {
+            _stateService = stateService;
+        }
+
+        [HttpGet("{id}", Name = "GetStateWithId")]
+        [Authorize("Bearer")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            if(id == Guid.Empty)
+                return BadRequest();
+
+            try
+            {
+                var result = await _stateService.Get(id);
+                if(result == null)
+                    return NotFound();
+
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Authorize("Bearer")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                return Ok(await _stateService.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
